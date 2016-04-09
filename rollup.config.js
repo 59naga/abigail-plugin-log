22510b684bcb7bc5be743c7007a11de001c9e198
59naga <i59naga@icloud.com>
2016-04-09 21:56:49 +0900

    fix: remove uglify, plugin name was broken

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,6 @@
 import nodeResolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
-import uglify from 'rollup-plugin-uglify';
 
 export default {
   entry: 'src/index.js',
@@ -12,6 +11,5 @@ export default {
     nodeResolve({ skip: ['abigail-plugin'] }),
     commonjs(),
     babel(),
-    uglify(),
   ],
 };
